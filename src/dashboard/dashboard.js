dab8645d4116372a40022f0336f242897ac6fb75
FIRST_NAME LAST_NAME <ayushverulkar5@gmail.com>
2022-11-21 01:52:12 +0530

    username api integrated

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,11 +1,43 @@
-const loadeUserProfile = ()=>{
+import { fetchRequest } from "../api";
+import { ENDPOINT, logout } from "../common";
+
+
+const onProfileClick = (event)=>{
+    event.stopPropagation();
+    const profileMenu = document.querySelector("#profile-menu");
+    profileMenu.classList.toggle("hidden");
+    if(!profileMenu.classList.contains("hidden")){
+        profileMenu.querySelector("li#logout").addEventListener("click",logout);
+    }
+}
+
+const loadeUserProfile = async()=>{
     const defaultImage = document.querySelector("#default-image");
     const profileButton = document.querySelector("#user-profile-btn");
     const displayNameElement = document.querySelector("#display-name");
+
+    const {display_name:displayName, images} = await fetchRequest(ENDPOINT.userInfo);
+    displayNameElement.textContent = displayName;
+    if(images?.length){
+        defaultImage.classList.add("hidden");
+    }else{
+        defaultImage.classList.remove("hidden");
+    }
+
+    profileButton.addEventListener("click",onProfileClick);
 
 }
 
 
 document.addEventListener("DOMContentLoaded",()=>{
     loadeUserProfile();
+    document.addEventListener("click",()=>{
+        const profileMenu = document.querySelector("#profile-menu");
+        if(!profileMenu.classList.contains("hidden")){
+            profileMenu.classList.add("hidden");
+        }
+
+    })
 })
+
+
