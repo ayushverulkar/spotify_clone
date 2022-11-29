c0166177a1c5001608148162fa9bf2bbda5184dd
FIRST_NAME LAST_NAME <ayushverulkar5@gmail.com>
2022-11-29 18:31:04 +0530

    finished

--- src/api.js
+++ src/api.js
@@ -2,31 +2,31 @@ import { ACCESS_TOKEN, EXPIRES_IN, logout, TOKEN_TYPE } from "./common";
 
 const BASE_API_URL = import.meta.env.VITE_API_BASE_URL;
 
-const getAccessToken = ()=>{
+
+const getAccessToken = () => {
+
     const accessToken = localStorage.getItem(ACCESS_TOKEN);
-    const tokenType = localStorage.getItem(TOKEN_TYPE);
     const expiresIn = localStorage.getItem(EXPIRES_IN);
-    if(Date.now()<expiresIn){
-        return {accessToken,tokenType}
-    }else{
+    const tokenType = localStorage.getItem(TOKEN_TYPE);
+    if (Date.now() < expiresIn) {
+        return { accessToken, tokenType }
+    } else {
         logout();
     }
 
 }
 
-const createAPIConfig = ({accessToken,tokenType})=>{
+const createAPIConfig = ({ accessToken, tokenType }, method = "GET") => {
     return {
-        headers:{
-            Authorization:`${tokenType} ${accessToken}`
+        headers: {
+            Authorization: `${tokenType} ${accessToken}`
         },
-        // method
+        method
     }
 }
 
-export const fetchRequest = async(endpoint)=>{
+export const fetchRequest = async (endpoint) => {
     const url = `${BASE_API_URL}/${endpoint}`;
-    const result = await fetch(url,createAPIConfig(getAccessToken()));
+    const result = await fetch(url, createAPIConfig(getAccessToken()));
     return result.json();
-                
-
 }
